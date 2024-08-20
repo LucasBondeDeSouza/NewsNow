6ab8239f69621c88da5c04bcb7ea22c6beee2247
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-08-20 18:39:53 -0300

    atualização

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,16 +1,10 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios"
-
-import clear from '../../public/images/clear.png'
-import cloud from '../../public/images/cloud.png'
-import rain from '../../public/images/rain.png'
-import snow from '../../public/images/snow.png'
+import axios from "axios";
 
 export default () => {
+    const [location, setLocation] = useState({ city: "", country: "", temp: null, weather: "", isDay: "" });
 
-    const [location, setLocation] = useState({ city: "", country: "", temp: null, weather: "", isDay: "" })
-
-    const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+    const API_URL = 'http://localhost:5000';
 
     useEffect(() => {
         const fetchWeather = async () => {
@@ -20,29 +14,36 @@ export default () => {
             } catch (err) {
                 console.error("Erro ao buscar dados meteorológicos:", err);
             }
-        }
+        };
 
-        fetchWeather()
-    }, [])
+        fetchWeather();
+
+        // Atualiza os dados a cada segundo
+        const intervalId = setInterval(() => {
+            fetchWeather();
+        }, 1000);
+
+        return () => clearInterval(intervalId); // Limpa o intervalo quando o componente é desmontado
+    }, []);
 
     const getWeatherIcon = (weather, isDay) => {
-        if (weather == 'Clear') {
-            return isDay == true ? <img src={clear} alt={weather} /> : <i class="bi bi-moon-fill text-warning fs-5 m-0"></i>
-        } else if (weather == 'Clouds') {
-            return isDay == true ? <img src={cloud} alt={weather} /> : <i class="bi bi-cloud-moon-fill text-warning fs-5 m-0"></i>
-        } else if (weather == 'Rain') {
-            return <img src={rain} alt={weather} />
-        } else if (weather == 'Thunderstorm') {
-            return <i class="bi bi-cloud-lightning-rain text-primary fs-4 m-0"></i>
-        } else if (weather == 'Snow') {
-            return <img src={snow} alt={weather} />
-        } 
-    }
+        if (weather === 'Clear') {
+            return isDay ? <i class="bi bi-brightness-high-fill text-warning fs-3 m-0"></i> : <i class="bi bi-moon-fill text-warning fs-4 m-0"></i>
+        } else if (weather === 'Clouds') {
+            return isDay ? <i class="bi bi-cloud-sun-fill fs-3 m-0 cloud-sun"></i> : <i class="bi bi-cloud-moon-fill fs-3 m-0 cloud-moon"></i>
+        } else if (weather === 'Rain') {
+            return <i class="bi bi-cloud-rain-fill fs-3 m-0 rain"></i>
+        } else if (weather === 'Thunderstorm') {
+            return <i class="bi bi-cloud-rain-heavy-fill fs-3 m-0 thunderstorm"></i>
+        } else if (weather === 'Snow') {
+            return <i class="bi bi-cloud-snow-fill fs-3 m-0 snow"></i>
+        }
+    };
 
     return (
         <header>
-            <div class="px-3 py-2 border-bottom mb-3">
-                <div class="container d-flex justify-content-between align-items-center">
+            <div className="px-3 py-2 border-bottom mb-3">
+                <div className="container d-flex justify-content-between align-items-center">
                     <div className="logo d-flex align-items-center gap-2">
                         <i className="bi bi-newspaper fs-2 m-0"></i>
                         <h5 className="d-none d-lg-inline m-0">NewsNow</h5>
@@ -62,5 +63,5 @@ export default () => {
                 </div>
             </div>
         </header>
-    )
-}
+    );
+};
