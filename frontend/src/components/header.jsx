5bb6392348f149b58b572eb0675accfff11a82ab
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-08-20 15:43:19 -0300

    atualização

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -10,7 +10,7 @@ export default () => {
 
     const [location, setLocation] = useState({ city: "", country: "", temp: null, weather: "", isDay: "" })
 
-    const API_URL = 'http://localhost:5000';
+    const API_URL = process.env.REACT_APP_API_URL;
 
     useEffect(() => {
         const fetchWeather = async () => {
